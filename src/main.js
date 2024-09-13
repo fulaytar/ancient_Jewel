30f706b2fd5947f1764d4f08849f9e13ba0e00ff
fulaytar <fulaytar2405@gmail.com>
2024-09-13 13:34:08 +0300

    initial commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './js/header';
+import './js/hero';
+import './js/about';
+import './js/reviews';
+import './js/howtoplay';
+import './js/gallery';
+import './js/advantages';
+import './js/disclaimer';
+import './js/footer';
