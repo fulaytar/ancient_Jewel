16873734a22844fdc0d0056121812b4387520388
fulaytar <fulaytar2405@gmail.com>
2024-09-18 02:05:53 +0300

    need update galerry

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,1 +1,31 @@
-console.log('gallery');
+import Swiper from 'swiper/bundle';
+import 'swiper/swiper-bundle.css';
+const swiper = new Swiper('.swiper', {
+  effect: 'coverflow',
+  loop: true,
+  centeredSlides: true,
+  slidesPerView: 2,
+  initialSlide: 1,
+  keyboardControl: true,
+  mousewheelControl: true,
+  lazyLoading: true,
+  centeredSlides: true,
+  preventClicks: false,
+  preventClicksPropagation: false,
+  lazyLoadingInPrevNext: true,
+  coverflow: {
+    rotate: 0,
+    stretch: 150,
+    depth: 200,
+    modifier: 1,
+    slideShadows: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true, // Enable clicking on pagination bullets
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
