3d88815f380a1a9b56148dc263703a7cee767aed
fulaytar <fulaytar2405@gmail.com>
2024-09-19 01:44:30 +0300

    update header

--- src/js/header.js
+++ src/js/header.js
@@ -1,13 +1,28 @@
-console.log('header');
+const burgerButton = document.getElementById('burger-button');
+const burgerModal = document.getElementById('burger-modal');
+const closeButton = document.getElementById('close-button');
 
-const burgerButton = document.getElementById('burger-button')
-const burgerModal = document.getElementById('burger-modal')
-const closeButton = document.getElementById('close-button')
+burgerButton.addEventListener('click', openModal);
+closeButton.addEventListener('click', closeModal);
 
-burgerButton.addEventListener('click', () => {
-    burgerModal.classList.remove('hidden')
-})
+function closeModal() {
+  burgerModal.classList.add('hidden');
+  burgerButton.classList.remove('hidden');
 
-closeButton.addEventListener('click', () => {
-    burgerModal.classList.add('hidden')
-})
+  // Видаляємо обробник події скролу при закритті
+  window.removeEventListener('scroll', handleScroll);
+}
+
+function openModal() {
+  burgerModal.classList.remove('hidden');
+  burgerButton.classList.add('hidden');
+
+  // Додаємо обробник події скролу
+  window.addEventListener('scroll', handleScroll);
+}
+
+function handleScroll() {
+  if (window.scrollY > 20) {
+    closeModal();
+  }
+}
