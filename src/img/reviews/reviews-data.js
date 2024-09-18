61854f26cbae2b445bf39c102bb5cd7436eaeb2c
Shekel Roman <romansekel37@gmail.com>
2024-09-18 23:55:32 +0200

    fix

--- src/img/reviews/reviews-data.js
+++ src/img/reviews/reviews-data.js
@@ -1,56 +1,56 @@
 const reviews = [
     {
-        img: './src/img/reviews/emma.png',
-        img2x: './src/img/reviews/emma@2x.png',
+        img: './img/reviews/emma.png',
+        img2x: './img/reviews/emma@2x.png',
         name: 'Emma W.',
         text: 'Ancient Jewel is a great game for relaxing after a long day. The soundtrack is soothing.',
         stars: 5
     },
     {
-        img: './src/img/reviews/lucas.png',
-        img2x: './src/img/reviews/lucas@2x.png',
+        img: './img/reviews/lucas.png',
+        img2x: './img/reviews/lucas@2x.png',
         name: 'Lucas T.',
         text: 'The variety of levels keeps the game interesting. I’m always looking forward to the next one.',
         stars: 5
     },
     {
-        img: './src/img/reviews/ava.png',
-        img2x: './src/img/reviews/ava@2x.png',
+        img: './img/reviews/ava.png',
+        img2x: './img/reviews/ava@2x.png',
         name: 'Ava R.',
         text: 'The daily rewards are great. They help me progress through the game.',
         stars: 5
     },
     {
-        img: './src/img/reviews/henry.png',
-        img2x: './src/img/reviews/henry@2x.png',
+        img: './img/reviews/henry.png',
+        img2x: './img/reviews/henry@2x.png',
         name: 'Henry F.',
         text: 'I love competing with my friends for the highest score. The social features are great!',
         stars: 5
     },
     {
-        img: './src/img/reviews/isabella.png',
-        img2x: './src/img/reviews/isabella@2x.png',
+        img: './img/reviews/isabella.png',
+        img2x: './img/reviews/isabella@2x.png',
         name: 'Isabella C.',
         text: 'The special events are exciting. I always look forward to them!',
         stars: 5
     },
     {
-        img: './src/img/reviews/ethan.png',
-        img2x: './src/img/reviews/ethan@2x.png',
+        img: './img/reviews/ethan.png',
+        img2x: './img/reviews/ethan@2x.png',
         name: 'Ethan G.',
         text: 'Ancient Jewel is the best match-3 game I’ve played. The ancient theme is really fun!',
         stars: 5
     },
     {
-        img: './src/img/reviews/mia.png',
-        img2x: './src/img/reviews/mia@2x.png',
+        img: './img/reviews/mia.png',
+        img2x: './img/reviews/mia@2x.png',
         name: 'Mia S.',
         text: 'The levels are challenging but not impossible. Perfect for any skill level!',
         stars: 5
     },
     {
-        img: './src/img/reviews/alexander.png',
-        img2x: './src/img/reviews/alexander@2x.png',
+        img: './img/reviews/alexander.png',
+        img2x: './img/reviews/alexander@2x.png',
         name: 'Alexander J.',
         text: "I can't get enough of Ancient Jewel. Every level is a new adventure!",
         stars: 5
