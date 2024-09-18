8b53ba64aebbc4328654a96cd95cd4865a270fcc
fulaytar <fulaytar2405@gmail.com>
2024-09-18 21:24:41 +0300

    add mobille gallery

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,31 +1,45 @@
 import Swiper from 'swiper/bundle';
 import 'swiper/swiper-bundle.css';
+
+// Спочатку відображаємо перший слайд
+document.getElementById('current-slide-number').textContent = 1;
+
 const swiper = new Swiper('.swiper', {
   effect: 'coverflow',
-  loop: true,
   centeredSlides: true,
-  slidesPerView: 2,
-  initialSlide: 1,
-  keyboardControl: true,
-  mousewheelControl: true,
-  lazyLoading: true,
-  centeredSlides: true,
-  preventClicks: false,
-  preventClicksPropagation: false,
-  lazyLoadingInPrevNext: true,
-  coverflow: {
+  slidesPerView: 1.38,
+  spaceBetween: -100,
+  initialSlide: 0, // Індекс починається з 0, але виводимо як 1
+  keyboard: {
+    enabled: true,
+  },
+  mousewheel: {
+    enabled: true,
+  },
+  lazy: {
+    loadPrevNext: true,
+  },
+  coverflowEffect: {
     rotate: 0,
-    stretch: 150,
+    stretch: 100,
     depth: 200,
     modifier: 1,
     slideShadows: false,
   },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true, // Enable clicking on pagination bullets
-  },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.my-swiper-button-next',
+    prevEl: '.my-swiper-button-prev',
+  },
+  on: {
+    init: function () {
+      // При ініціалізації отримуємо номер слайда через this
+      document.getElementById('current-slide-number').textContent =
+        this.realIndex + 1;
+    },
+    slideChange: function () {
+      // Оновлюємо номер слайда через this при зміні
+      document.getElementById('current-slide-number').textContent =
+        this.realIndex + 1;
+    },
   },
 });
