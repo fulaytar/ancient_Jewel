c213cafbae6cad2b85fc52d8aca4af3f011c9b7a
Shekel Roman <romansekel37@gmail.com>
2024-09-18 23:06:35 +0200

    adv page

--- src/js/advantages.js
+++ src/js/advantages.js
@@ -1,1 +1,26 @@
 console.log('advantages');
+
+const left = document.getElementById('left-btn')
+const right = document.getElementById('right-btn')
+const list = document.getElementById('list')
+const page = document.getElementById('page')
+
+let currentPage = 0;
+
+right.addEventListener('click', () => {
+    if (currentPage < 1) {
+        currentPage++;
+        list.style.transform = `translateX(-${currentPage * 300}px)`;
+        page.textContent = '02'
+      }
+})
+
+left.addEventListener('click', () => {
+    if (currentPage > 0) {
+        currentPage--;
+        list.style.transform = `translateX(-${currentPage * 300}px)`;
+        page.textContent = '01'
+      }
+})
+
+
